bb554b3ebff0ac3202f95b9b15afc1721f95587d
Simone C <simone.comari@datasensing.com>
2023-11-30 16:13:45 +0100

    fixed issue #4

--- pyVNC/pyvnc.py
+++ pyVNC/pyvnc.py
@@ -6,7 +6,7 @@ from pyVNC.Client import Client
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", default="127.0.0.1", type=str, help="Hostname of the VNC Server")
-    parser.add_argument("--port", default="5902", type=int, help="VNC Server Port")
+    parser.add_argument("--port", default="5900", type=int, help="VNC Server Port")
     parser.add_argument("--password", default=None, type=str, help="Password of the VNC Server")
     parser.add_argument("--depth", default=32, type=int, help="Color Depth")
     parser.add_argument("--fast", default=False, type=bool,  help="Fast encoding")
@@ -20,7 +20,7 @@ if __name__ == '__main__':
                     fast=args.fast,
                     shared=args.shared,
                     gui=True,
-                    gui_with_array=False
+                    array=False
                  )
     vnc.start()
 
